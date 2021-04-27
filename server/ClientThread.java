0ac70d66a6ea192b645f0e564d9d6c8063b79c18
Bechir-Brahem <bb@pop-os.localdomain>
2021-04-27 18:12:55 +0100

    added history but synch error

--- server/ClientThread.java
+++ server/ClientThread.java
@@ -2,10 +2,16 @@ package server;
 
 import java.io.*;
 import java.net.*;
+import java.util.Vector;
 
 public class ClientThread extends Thread {
     Socket s;
     Color c;
+    Vector<String> history;
+    int history_length = 50;
+    int history_cur = 0;
+    int history_reached = 0;
+
 
     ClientThread(Socket s) {
         this.s = s;
@@ -20,17 +26,14 @@ public class ClientThread extends Thread {
             sendToClients(username + " hopped in the conversation!");
             // displays connected users only to a newly connected user
             client.send("connected users: ");
-            String tmp = "";
-            for (var tmp_client : Server.clients) {
-                tmp += tmp_client.username + " ";
-            }
-            client.send(tmp);
+            client.send(show_connected());
+            client.send(history_print());
 
             //lisetener
             while (true) {
                 try {
                     String msg = client.listen();
-                    sendToClients(username + "> " + msg);
+                    sendToClients("[" + username + "] " + msg);
                 } catch (Exception e) {
                     Server.clients.remove(client);
                     sendToClients(username + " has disconnected");
@@ -42,11 +45,35 @@ public class ClientThread extends Thread {
             e.printStackTrace();
         }
     }
+    private synchronized String history_print() {
+        String tmp = "";
+        for (var x : history) {
+            tmp += x + "\n";
+        }
+        return tmp;
+    }
+    private synchronized void history_add_message(String msg) {
+        if (history_reached < history_length) {
+            history.add(msg);
+            history_reached++;
+        } else {
+            history.set(history_cur, msg);
+            history_cur = (history_cur + 1) % history_length;
+        }
+    }
+    private synchronized String show_connected() {
+        String tmp = "";
+        for (var tmp_client : Server.clients) {
+            tmp += tmp_client.username + " ";
+        }
+        return tmp;
+    }
 
-    private void sendToClients(String msg) {
+    private synchronized void sendToClients(String msg) {
         for (var client : Server.clients) {
             try {
                 client.send(c.getCode() + msg + Color.RESET.getCode());
+                history_add_message(msg);
             } catch (Exception e) {
                 String username = client.username;
                 Server.clients.remove(client);
